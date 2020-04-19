0ade2da8b4ff08a9eeeff5d37a613c0b13efefaa
lstojke <lukaszstojke@gmail.com>
2020-04-19 20:39:32 +0200

    final version

--- lab_2/src/dh_converter.py
+++ lab_2/src/dh_converter.py
@@ -4,10 +4,10 @@ from tf.transformations import *
 import json
 
 def dh_to_urdf():
-	json_reader = open('/home/lukasz/catkin_ws/src/kostrzenski_stojke/lab_2/yaml/dh_params.json', 'r')
+	json_reader = open('../yaml/dh_params.json', 'r')
 	dh_params = json.loads(json_reader.read())
 
-	urdf_file_writer = open('/home/lukasz/catkin_ws/src/kostrzenski_stojke/lab_2/yaml/urdf.yaml', 'w')
+	urdf_file_writer = open('../yaml/urdf.yaml', 'w')
 	axis_x = (1, 0, 0)
 	axis_z = (0, 0, 1)
 
@@ -25,11 +25,11 @@ def dh_to_urdf():
 		xyz_params = translation_from_matrix(dh)
 
 		urdf_file_writer.write(key + ": \n")
-		urdf_file_writer.write("	joint_xyz: {} {} {} \n".format(*xyz_params))
-		urdf_file_writer.write("	joint_rpy: {} {} {} \n".format(*rpy_params))
-		urdf_file_writer.write("	link_xyz: {} 0 0 \n".format(xyz_params[0]/2))
-		urdf_file_writer.write("	link_rpy: 0 0 0 \n")
-		urdf_file_writer.write("	link_length: {} \n\n".format(a))
+		urdf_file_writer.write(" joint_xyz: {} {} {} \n".format(*xyz_params))
+		urdf_file_writer.write(" joint_rpy: {} {} {} \n".format(*rpy_params))
+		urdf_file_writer.write(" link_xyz: {} 0 0 \n".format(xyz_params[0]/2))
+		urdf_file_writer.write(" link_rpy: 0 0 0 \n")
+		urdf_file_writer.write(" link_length: {} \n\n".format(a))
 
 if __name__ == '__main__':
     dh_to_urdf()
